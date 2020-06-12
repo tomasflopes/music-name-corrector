389c4c40c37854f851e6ccb2d00d20c0353705c0
Tomás Lopes <tomas050302@gmail.com>
2020-06-12 15:44:56 +0100

    feat(backend): finished logic to correct names

--- backend/routes.js
+++ backend/routes.js
@@ -1,35 +1,27 @@
 const { Router } = require('express');
 const path = require('path');
 const fs = require('fs');
-const multer = require('multer');
 
 const routes = Router();
 
-const upload = multer({ storage: storage }).array('file')
+routes.post('/main', (request, response) => {
+  const { dir } = request.body;
 
-routes.get('/main', (request, response) => {
-  upload(request, response, (error) => {
-    return response.status(400).json(error)
-  });
-
-  console.log(request.file);
-  const filePath = path.join(__dirname, '..', 'example');
-
-  fs.readdir(filePath, { encoding: 'utf-8' }, function (err, data) {
+  fs.readdir(dir, { encoding: 'utf-8' }, function (err, data) {
     if (!err) {
       const dataArray = data.map(item => item.split(' '));
 
       const sanitizedData = dataArray.map(music =>
         music.map(word => {
+          console.log(word);
+          if (word === '') return;
           if (word === '-') return word;
           if (word.endsWith('-')) return word.replace('-', '') + ' -';
           if (word.startsWith('-')) return '- ' + word.replace('-', '');
           if (word.toLowerCase() === 'feat.') return word.toLowerCase();
-          if (word[0] === word[0].toLowerCase() && !word.match(/[0-9]-/)) {
-            const firstLetter = word.substr(0, 1).toUpperCase();
-            return firstLetter + word.substr(1, word.length);
-          }
-          return word;
+          if (word.match(/[0-9]-/)) return word;
+          const firstLetter = word.substr(0, 1).toUpperCase();
+          return firstLetter + word.substr(1, word.length).toLowerCase();
         })
       );
 
@@ -44,6 +36,8 @@ routes.get('/main', (request, response) => {
       }
 
       return response.json(finalData);
+    } else {
+      return response.status(400).json(err);
     }
   });
 });
