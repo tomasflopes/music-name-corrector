b05b952aefd1a9efc298859dfc6a8b318a7d75ed
Tomás Lopes <tomas050302@gmail.com>
2020-06-13 09:06:52 +0100

    feat(backend): added exceptions and fixed bugs

--- backend/routes.js
+++ backend/routes.js
@@ -7,46 +7,56 @@ const routes = Router();
 routes.post('/main', (request, response) => {
   const { dir } = request.body;
 
-  fs.readdir(dir, { encoding: 'utf-8' }, function (err, data) {
-    if (!err) {
-      const dataArray = data.map(item => item.split(' '));
+  let finalData = [];
 
-      const sanitizedData = dataArray.map(music =>
-        music.map(word => {
-          console.log(word);
-          if (word === '') return;
-          if (word === '-') return word;
-          if (word.toLowerCase() === 'deau') return 'DEAU';
-          if (word.toLowerCase() === 'kaines') return 'kAINES';
-          if (word.toLowerCase() === 's.e.f.') return 'S.E.F.';
-          if (word.endsWith('-')) return word.replace('-', '') + ' -';
-          if (word.startsWith('-')) return '- ' + word.replace('-', '');
-          if (word.toLowerCase() === 'feat.') return word.toLowerCase();
-          if (word.match(/[0-9]-/)) return word;
-          if (word.startsWith('(')) {
-            const firstLetter = word.substr(1, 1).toUpperCase();
-            return '(' + firstLetter + word.substr(2, word.length).toLowerCase();
-          }
-          const firstLetter = word.substr(0, 1).toUpperCase();
-          return firstLetter + word.substr(1, word.length).toLowerCase();
-        })
-      );
+  for (let i = 0; i < 2; i++) {
+    fs.readdir(dir, { encoding: 'utf-8' }, function (err, data) {
+      if (!err) {
+        const dataArray = data.map(item => item.split(' '));
 
-      const finalData = sanitizedData.map(nameArray => {
-        return nameArray.join(' ');
-      });
+        const sanitizedData = dataArray.map(music =>
+          music.map(word => {
+            console.log(word);
+            if (word === '') return;
+            if (word === '-') return word;
+            if (word.toLowerCase() === 'deau') return 'DEAU';
+            if (word.toLowerCase() === 'kaines') return 'kAINES';
+            if (word.toLowerCase() === 's.e.f.') return 'S.E.F.';
+            if (word.toLowerCase() === 'profjam') return 'ProfJam';
+            if (word.toLowerCase() === 'xtinto') return 'XTinto';
+            if (word.toLowerCase() === 'x-tense') return 'X-Tense';
+            if (word.toLowerCase() === 'rdc') return 'RDC';
+            if (word.toLowerCase() === 'gson') return 'GSon';
+            if (word.toLowerCase() === 'grognation') return 'GROGNation';
+            if (word.endsWith('-')) return word.replace('-', '') + ' -';
+            if (word.startsWith('-')) return '- ' + word.replace('-', '');
+            if (word.toLowerCase() === 'feat.') return word.toLowerCase();
+            if (word.match(/[0-9]-/)) return word;
+            if (word.startsWith('(')) {
+              const firstLetter = word.substr(1, 1).toUpperCase();
+              return '(' + firstLetter + word.substr(2, word.length).toLowerCase();
+            }
+            const firstLetter = word.substr(0, 1).toUpperCase();
+            return firstLetter + word.substr(1, word.length).toLowerCase();
+          })
+        );
 
-      for (let i = 0; i < finalData.length; i++) {
-        fs.rename(path.join(__dirname, '..', 'example', data[i]), path.join(__dirname, '..', 'example', finalData[i]), function (err) {
-          if (err) console.log('ERROR: ' + err);
+        finalData = sanitizedData.map(nameArray => {
+          return nameArray.join(' ');
         });
+
+        for (let i = 0; i < finalData.length; i++) {
+          fs.rename(`${dir}/${data[i]}`, `${dir}/${finalData[i]}`, function (err) {
+            if (err) console.log('ERROR: ' + err);
+          });
+        }
+      } else {
+        return response.status(400).json(err);
       }
 
       return response.json(finalData);
-    } else {
-      return response.status(400).json(err);
-    }
-  });
+    });
+  }
 });
 
 module.exports = routes;
