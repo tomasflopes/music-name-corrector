7bbcf48f2a999f8b336d82e4ceb583551a169a8a
Tomás Lopes <tomas050302@gmail.com>
2020-07-17 18:47:28 +0100

    feat: added more exceptions

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const routes = require('./routes');
-const express = require('express');
-const cors = require('cors');
+const routes = require("./routes");
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes);
